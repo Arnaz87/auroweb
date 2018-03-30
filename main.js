18872510e8b183a4058c8bb58820c616cedebbb7
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2018-03-30 02:02:08 -0400

    Cobre 0.5, correctly compiles and run culang.lexer and culang.parser, working on culang.compiler

--- main.js
+++ main.js
@@ -1,220 +1,7 @@
 
 const fs = require("fs");
 
-function parse (buffer) {
-  buffer = new Uint8Array(buffer);
-  var pos = 0;
-
-  function fail (msg) { throw new Error(msg + ". at byte " + pos); }
-  function unsupported (msg) { fail("Unsupported " + msg); }
-
-  function readByte () {
-    if (pos >= buffer.length)
-      fail("Unexpected end of file");
-    return buffer[pos++];
-  }
-
-  function readInt () {
-    var n = 0;
-    var b = readByte();
-    while (b & 0x80) {
-      n = (n << 7) | (b & 0x7f);
-      b = readByte();
-    }
-    return (n << 7) | (b & 0x7f);
-  }
-
-  function readStr () {
-    var len = readInt();
-    var str = "";
-    while (len > 0) {
-      byte = readByte();
-      str += String.fromCharCode(byte);
-      len--;
-    }
-    return str;
-  }
-
-  function parseN (n, f) {
-    var arr = [];
-    for (var i = 0; i < n; i++)
-      arr.push(f());
-    return arr;
-  }
-
-  function readInts (n) { return parseN(n, readInt); }
-
-  var magic = "";
-  while (true) {
-    var byte = readByte();
-    if (byte == 0) break;
-    magic += String.fromCharCode(byte);
-  }
-  if (magic !== "Cobre ~4") fail("Not a \"Cobre ~4\" module.");
-
-  var modules = parseN(readInt(), function () {
-    var k = readInt();
-    switch (k) {
-      case 0: return {
-        type: "import",
-        name: readStr(),
-      };
-      case 1: return {
-        type: "define",
-        items: parseN(readInt(), function () {
-          var types = ["module", "type", "function", "const"];
-          var k = readInt();
-          if (k > 3) fail("Unknown item kind " + k);
-          return {
-            type: types[k],
-            index: readInt(),
-            name: readStr(),
-          }
-        })
-      };
-      case 2: return {
-        type: "functor",
-        name: readStr(),
-      };
-      case 3: return {
-        type: "use",
-        module: readInt(),
-        item: readStr(),
-      };
-      case 4: return {
-        type: "build",
-        base: readInt(),
-        argument: readInt(),
-      };
-      default: fail("Unknown import kind " + k);
-    }
-  });
-
-  var types = parseN(readInt(), function () {
-    var k = readInt();
-    if (k == 0) fail("Null type");
-    if (k != 1) fail("Unknown type kind " + k);
-    return {
-      type: "import",
-      module: readInt(),
-      name: readStr(),
-    };
-  });
-
-  var functions = parseN(readInt(), function () {
-    var k = readInt();
-    var f;
-    switch (k) {
-      case 0: fail("Null function");
-      case 1: f = {
-        type: "import",
-        module: readInt(),
-        name: readStr(),
-      }; break;
-      case 2: f = {
-        type: "code"
-      }; break;
-      default: fail("Unknown function kind " + k);
-    }
-    f.ins = parseN(readInt(), readInt);
-    f.outs = parseN(readInt(), readInt);
-    return f;
-  });
-
-  var statics = parseN(readInt(), function () {
-    var k = readInt();
-    switch (k) {
-      case 0: fail("Null kind static");
-      case 1: return {
-        type: "import",
-        module: readInt(),
-        name: readStr(),
-      };
-      case 2: return {type: "int", value: readInt()};
-      case 3:
-        var len = readInt();
-        var arr = [];
-        for (var i = 0; i < len; i++)
-          arr.push(readInt());
-        return {
-          type: "bin",
-          data: arr,
-        };
-      case 4: return {type: "type", index: readInt()};
-      case 5: return {type: "function", index: readInt()};
-    }
-    if (k < 16) fail("Unknown static kind " + k);
-    return {type: "null", index: k-16};
-  });
-
-  function parseCode (fn) {
-    function one (tp) { return {type: tp, a: readInt()}; }
-    function two (tp) { return {type: tp, a: readInt(), b: readInt()}; }
-    var count = readInt();
-    return parseN(count, function () {
-      var k = readInt();
-      switch (k) {
-        case 0: return {
-          type: "end",
-          args: readInts(fn.outs.length)
-        };
-        case 1: return {type: "var"};
-        case 2: return one("dup");
-        case 3: return two("set");
-        case 4: return one("sgt");
-        case 5: return two("sst");
-        case 6: return one("jmp");
-        case 7: return two("jif");
-        case 8: return two("nif");
-      }
-      if (k < 16) fail("Unknown instruction " + k);
-      var ix = k-16;
-      if (ix >= functions.length)
-        fail("Function index out of bounds");
-      var ff = functions[ix];
-      return {
-        type: "call",
-        index: ix,
-        args: readInts(ff.ins.length),
-      }
-    });
-  }
-  
-  for (var i in functions) {
-    var fn = functions[i];
-    if (fn.type != "code") continue;
-    fn.code = parseCode(fn);
-  }
-  var static = {ins: [], outs: []};
-  static.code = parseCode(static);
-
-  function parseNode () {
-    var n = readInt();
-    if (n & 1) return n>>1;
-    if (n & 2) {
-      n = n >> 2;
-      var str = "";
-      while (n > 0) {
-        byte = readByte();
-        str += String.fromCharCode(byte);
-        n--;
-      }
-      return str;
-    }
-    return parseN(n>>2, parseNode);
-  }
-
-  var metadata = parseNode();
-
-  return {
-    modules: modules,
-    types: types,
-    functions: functions,
-    statics: statics,
-    static: static,
-    metadata: metadata,
-  };
-}
+const parse = require("./parse.js");
 
 var output = "";
 
@@ -242,7 +29,7 @@ function BaseModule (data) {
   }
 }
 
-function Parsed (parsed) {
+function Parsed (parsed, modulename) {
   var modcache = {};
 
   var sourcemap = {};
@@ -277,7 +64,7 @@ function Parsed (parsed) {
         if (item.type == "type")
           return get_type(item.index);
       };
-      mod.build = function () { return mod; }
+      mod.build = function () { throw new Exception("module is not a functor"); }
       modcache[n] = mod;
       return mod;
     }
@@ -288,20 +75,48 @@ function Parsed (parsed) {
   function get_function (n) {
     if (funcache[n]) return funcache[n];
     var fn = parsed.functions[n];
+    var f;
     if (fn.type == "import") {
       var mod = get_module(fn.module);
-      var f = mod.get(fn.name);
+      f = mod.get(fn.name);
       if (!f) throw new Error("No item " + fn.name + " found in module");
-      funcache[n] = f;
-      return f;
     } else if (fn.type == "code") {
-      var f = new Code(fn, n);
+      f = new Code(fn, n);
       toCompile.push(f);
-      funcache[n] = f;
-      return f;
+    } else if (fn.type == "int") {
+      f = macro(String(fn.value), 0, 1);
+    } else if (fn.type == "bin") {
+      var str = "\"";
+      for (var j = 0; j < fn.data.length; j++) {
+        var char = String.fromCharCode(fn.data[j]);
+        if (char == '"') char = "\\\"";
+        if (char == "\n") char = "\\n";
+        str += char;
+      }
+      str += "\"";
+      f = macro(str, 0, 1);
+    } else if (fn.type == "call") {
+      var cfn = get_function(fn.index);
+      var args = fn.args.map(function (ix) {
+        return get_function(ix).use([]);
+      });
+      var expr = cfn.use(args);
+      if (cfn.pure) {
+        f = macro(expr, 0, 1);
+      } else {
+        var name = "cns" + toCompile.length;
+        toCompile.push({
+          compile: function () {
+            putln("var " + name + " = " + expr + ";")
+          }
+        });
+        f = {ins: [], outs: [-1], use: function () {return name;}};
+      }
     } else {
       throw new Error("Unsupported function kind " + fn.type);
     }
+    funcache[n] = f;
+    return f;
   }
 
   var tpcache = {};
@@ -312,31 +127,6 @@ function Parsed (parsed) {
     var t = mod.get(tp.name);
     tpcache[n] = t;
     return t;
-  }
-
-  var cnscache = [];
-  function Constant (cns) {
-    this.name = "cns" + toCompile.length;
-    this.compile = function () {
-      var ln = "var " + this.name + " = ";
-      if (cns.type == "int") ln += cns.value;
-      if (cns.type == "bin") {
-        ln += "\"";
-        for (var j = 0; j < cns.data.length; j++)
-          ln += String.fromCharCode(cns.data[j]);
-        ln += "\"";
-      }
-      if (cns.type == "null") ln += "null";
-      putln(ln + ";");
-    }
-  }
-
-
-
-  for (var i = 0; i < parsed.statics.length; i++) {
-    var cns = new Constant(parsed.statics[i]);
-    toCompile.push(cns);
-    cnscache.push(cns);
   }
 
   function Code (fn, index) {
@@ -354,7 +144,6 @@ function Parsed (parsed) {
         var k = inst.type;
         if (k == "var") regs++;
         if (k == "dup") inst.out = regs++;
-        if (k == "sgt") inst.out = regs++;
         if (k == "jmp") lbls.push(inst.a);
         if (k == "jif") lbls.push(inst.a);
         if (k == "nif") lbls.push(inst.a);
@@ -375,7 +164,6 @@ function Parsed (parsed) {
       fn.code[0].lbl = true;
 
       var args = "";
-      var first = true;
       for (var i = 0; i < fn.ins.length; i++) {
         if (i>0) args += ", ";
         args += "reg_" + i;
@@ -404,34 +192,31 @@ function Parsed (parsed) {
         var k = inst.type;
         if (k == "dup") putln("  " + reg(inst.out) + " = " + reg(inst.a) + ";");
         if (k == "set") putln("  " + reg(inst.a) + " = " + reg(inst.b) + ";");
-        if (k == "sgt") putln("  " + reg(inst.out) + " = " + cnscache[inst.a].name + ";");
-        if (k == "sst") putln("  " + cnscache[inst.a].name + " = " + reg(inst.b) + ";");
         if (k == "jmp") putln("  _lbl = " + inst.a + "; break;");
         if (k == "jif") putln("  if (" + reg(inst.b) + ") { _lbl = " + inst.a + "; break; }");
         if (k == "nif") putln("  if (!" + reg(inst.b) + ") { _lbl = " + inst.a + "; break; }");
         if (k == "call") {
           var ff = get_function(inst.index);
-          if (ff.type == "macro") {
-            var call = ff.macro.replace("$1", reg(inst.args[0]));
-            if (inst.args.length > 1)
-              call = call.replace("$2", reg(inst.args[1]));
-            if (inst.outs.length == 0) putln("  " + call + ";");
-            else  putln("  " + reg(inst.outs[0]) + " = " + call + ";");
-          } else {
-            var args = "";
-            for (var j = 0; j < inst.args.length; j++) {
-              if (j > 0) args += ", ";
-              args += reg(inst.args[j]);
-            }
-            putln("  _result = " + ff.name + "(" + args + ");")
-            for (var j = 0; j < inst.outs.length; j++) {
-              putln("  " + reg(inst.outs[j]) + " = _result[" + j + "];");
-            }
+          var args = inst.args.map(function (x) {return reg(x);});
+          var expr = ff.use(args);
+          switch (inst.outs.length) {
+            case 0: putln("  " + expr + ";"); break;
+            case 1: putln("  " + reg(inst.outs[0]) + " = " + expr + ";"); break;
+            default:
+              putln("  _result = " + expr + ";")
+              for (var j = 0; j < inst.outs.length; j++) {
+                putln("  " + reg(inst.outs[j]) + " = _result[" + j + "];");
+              }
           }
         }
         if (k == "end") {
-          var args = inst.args.map(function (a) {return "reg_" + a;}).join(",");
-          putln("  return [" + args + "];");
+          var expr;
+          switch (inst.args.length) {
+            case 0: expr = "";
+            case 1: expr = reg(inst.args[0]);
+            default: expr = inst.args.map(function (a) {return reg(a);}).join(",");
+          }
+          putln("  return " + expr + ";");
         }
       }
 
@@ -439,6 +224,10 @@ function Parsed (parsed) {
       //putln("  throw new Error('Function does not return');")
       putln("  }");
       putln("}");
+    }
+
+    this.use = function (args) {
+      return this.name + "(" + args.join(", ") + ");"
     }
   }
 
@@ -469,10 +258,6 @@ function Parsed (parsed) {
     }
   }
 
-  var st = new Code(parsed.static);
-  toCompile.push(st);
-  toRun.push(st);
-
   var mod = get_module(1);
 
   /*mod.compile = function () {
@@ -487,6 +272,14 @@ function Parsed (parsed) {
 function macro (str, inc, outc) { return {
   type: "macro", macro: str,
   ins: new Array(inc), outs: new Array(outc),
+  use: function (args) {
+    var expr = this.macro;
+    for (var i = 0; i < this.ins.length; i++) {
+      var patt = new RegExp("\\$" + (i+1), "g");
+      expr = expr.replace(patt, args[i]);
+    }
+    return expr;
+  }
 }; }
 
 var recordcache = {};
@@ -520,9 +313,11 @@ var modules = {
     "new": macro("$1", 1, 1),
     "itos": macro("String($1)", 1, 1),
     "concat": macro("($1 + $2)", 2, 1),
+    "add": macro("($1 + $2)", 2, 1),
     "eq": macro("($1 == $2)", 2, 1),
     "length": macro("$1.length", 1, 1),
-    "charat": macro("$1[$2]", 2, 1),
+    "charat": macro("[$1[$2], $2+1]", 2, 2),
+    "newchar": macro("String.fromCharCode($1)", 1, 1),
     "codeof": macro("$1.charCodeAt(0)", 1, 1),
   }),
   "cobre.array": {build: function (arg) {
@@ -532,6 +327,7 @@ var modules = {
     var tp = newType("array(" + base.name + ")");
     mod = new BaseModule({
       "": tp,
+      "new": macro("new Array($2).fill($1)", 2, 1),
       "empty": macro("[]", 0, 1),
       "get": macro("$1[$2]", 2, 1),
       "set": macro("$1[$2]=$3", 3, 0),
@@ -549,6 +345,17 @@ var modules = {
       if (name == "test") return macro("($1.tp == " + id + ")", 1, 1);
       if (name == "get") return macro("$1.val", 1, 1);
     } };
+  } },
+  "cobre.null": { build: function (arg) {
+    var base = arg.get("0");
+    var tp = newType("null(" + base.name + ")");
+    return new BaseModule({
+      "": tp,
+      "null": macro("null", 0, 1),
+      "new": macro("$1", 1, 1),
+      "get": macro("$1", 1, 1),
+      "isnull": macro("($1 === null)", 1, 1),
+    });
   } },
   "cobre.record": { build: function (arg) {
     var arr = [];
@@ -570,7 +377,9 @@ var modules = {
 
     mod = { "get": function (name) {
       if (name == "new") {
-        return {type: "code", name: "_record", ins: [], outs: [0]};
+        return {ins: [], outs: [0], use: function (args) {
+          return "[" + args.join(", ") + "]";
+        }};
       }
       var a = name.slice(0, 3);
       var n = name.slice(3);
@@ -583,19 +392,20 @@ var modules = {
     return mod;
   } },
   "cobre.typeshell": {build: function (arg) {
-    var base = arg.get("0");
-    var tp = newType("typeshell(" + base.name + ")");
+    // Each time it's called, a new type is created
     return new BaseModule({
-      "": tp,
+      "": newType("typeshell"),
       "new": macro("$1", 1, 1),
       "get": macro("$1", 1, 1),
     });
   } },
 };
 
+modules["cobre.string"].data["new"].pure = true;
+
 function readModule (filename, name) {
   var data = parse(fs.readFileSync(filename));
-  var mod = Parsed(data);
+  var mod = Parsed(data, name);
   modules[name] = mod;
   return mod;
 }
@@ -604,10 +414,10 @@ function load_module (name) {
   if (modules[name] !== undefined) return modules[name];
 
   var filename = "./" + name;
-  if (fs.existsSync(filename)) return readModule(filename);
+  if (fs.existsSync(filename)) return readModule(filename, name);
 
   filename = process.env.HOME + "/.cobre/modules/" + name;
-  if (fs.existsSync(filename)) return readModule(filename);
+  if (fs.existsSync(filename)) return readModule(filename, name);
 
   throw new Error("Cannot load module " + name);
 }
@@ -681,10 +491,28 @@ if (mode == "term") {
   putln("function _print (line) { document.getElementById('content').textContent += line + '\\n'; }");
 }
 
-var mainmodule = readModule(modname);
+if (mode == "node") {
+  var orig = modules["cobre.system"].data;
+  modules["cobre.system"] = new BaseModule({
+    print: orig.print,
+    error: orig.error,
+    file: newType("file"),
+    readall: macro("fs.readFileSync($1, 'utf8')", 1, 1),
+    quit: macro("process.exit($1)", 1, 0),
+    argc: macro("argv.length", 0, 1),
+    args: macro("argv[$1]", 1, 1),
+    open: macro("fs.openSync($1, $2)", 2, 1),
+    write: macro("fs.writeSync($1, $2)", 2, 0),
+    writebyte: macro("fs.writeSync($1, Buffer.from([$2]))", 2, 0),
+  });
+  putln("var argv = process.argv.slice(1);")
+  putln("const fs = require('fs');");
+  putln("function readByte (f) {}");
+}
+
+var mainmodule = load_module(modname);
 var mainfn = mainmodule.get("main");
 
-putln("function _record () { return [arguments]; }");
 putln("function _error (msg) { throw new Error(msg); }");
 
 for (var i = 0; i < toCompile.length; i++) {
---
+++ parse.js
@@ -0,0 +1,215 @@
+module.exports = function parse (buffer) {
+
+buffer = new Uint8Array(buffer);
+var pos = 0;
+
+function fail (msg) { throw new Error(msg + ". at byte " + pos); }
+function unsupported (msg) { fail("Unsupported " + msg); }
+
+function readByte () {
+  if (pos >= buffer.length)
+    fail("Unexpected end of file");
+  return buffer[pos++];
+}
+
+function readInt () {
+  var n = 0;
+  var b = readByte();
+  while ((b & 0x80) > 0) {
+    n = (n << 7) | (b & 0x7f);
+    b = readByte();
+  }
+  return (n << 7) | (b & 0x7f);
+}
+
+function readStr () {
+  var len = readInt();
+  var str = "";
+  while (len > 0) {
+    byte = readByte();
+    str += String.fromCharCode(byte);
+    len--;
+  }
+  return str;
+}
+
+function parseN (n, f) {
+  var arr = [];
+  for (var i = 0; i < n; i++)
+    arr.push(f());
+  return arr;
+}
+
+function readInts (n) { return parseN(n, readInt); }
+
+var magic = "";
+while (true) {
+  var byte = readByte();
+  if (byte == 0) break;
+  magic += String.fromCharCode(byte);
+}
+if (magic !== "Cobre 0.5") fail("Not a \"Cobre 0.5\" module.");
+
+var modules = parseN(readInt(), function () {
+  var k = readInt();
+  switch (k) {
+    case 0: return {
+      type: "import",
+      name: readStr(),
+    };
+    case 1: return {
+      type: "define",
+      items: parseN(readInt(), function () {
+        var types = ["module", "type", "function", "const"];
+        var k = readInt();
+        if (k > 3) fail("Unknown item kind " + k);
+        return {
+          type: types[k],
+          index: readInt(),
+          name: readStr(),
+        }
+      })
+    };
+    case 2: return {
+      type: "functor",
+      name: readStr(),
+    };
+    case 3: return {
+      type: "use",
+      module: readInt(),
+      item: readStr(),
+    };
+    case 4: return {
+      type: "build",
+      base: readInt(),
+      argument: readInt(),
+    };
+    default: fail("Unknown import kind " + k);
+  }
+});
+
+var types = parseN(readInt(), function () {
+  var k = readInt();
+  if (k == 0) fail("Null type");
+  return {
+    type: "import",
+    module: k-1,
+    name: readStr(),
+  };
+});
+
+var functions = parseN(readInt(), function () {
+  var k = readInt();
+  var f;
+  switch (k) {
+    case 0: fail("Null function");
+    case 1: f = {
+      type: "code"
+    }; break;
+    default: f = {
+      type: "import",
+      module: k-2,
+    };
+  }
+  f.ins = parseN(readInt(), readInt);
+  f.outs = parseN(readInt(), readInt);
+  if (k>1) f.name = readStr();
+  return f;
+});
+
+var constCount = readInt();
+for (var i = 0; i < constCount; i++) {
+  var f;
+  var k = readInt();
+  if (k == 1) {
+    f = {type: "int", value: readInt()};
+  } else if (k == 2) {
+    var len = readInt();
+    var arr = [];
+    for (var j = 0; j < len; j++)
+      arr.push(readInt());
+    f = {
+      type: "bin",
+      data: arr,
+    };
+  } else if (k < 16) {
+    fail("Unknown constant kind " + k);
+  } else {
+    var ix = k-16;
+    // Functions not yet in the function list are constants
+    var argcount = (ix >= functions.length)? 0 : functions[ix].ins.length;
+    f = {
+      type: "call",
+      index: ix,
+      args: readInts(argcount),
+    };
+  }
+  f.ins = [];
+  f.outs = [-1];
+  functions.push(f);
+}
+
+function parseCode (fn) {
+  function one (tp) { return {type: tp, a: readInt()}; }
+  function two (tp) { return {type: tp, a: readInt(), b: readInt()}; }
+  var count = readInt();
+  return parseN(count, function () {
+    var k = readInt();
+    switch (k) {
+      case 0: return {
+        type: "end",
+        args: readInts(fn.outs.length)
+      };
+      case 1: return {type: "hlt"};
+      case 2: return {type: "var"};
+      case 3: return one("dup");
+      case 4: return two("set");
+      case 5: return one("jmp");
+      case 6: return two("jif");
+      case 7: return two("nif");
+    }
+    if (k < 16) fail("Unknown instruction " + k);
+    var ix = k-16;
+    if (ix >= functions.length)
+      fail("Function index out of bounds");
+    var ff = functions[ix];
+    return {
+      type: "call",
+      index: ix,
+      args: readInts(ff.ins.length),
+    }
+  });
+}
+
+for (var i = 0; i < functions.length; i++) {
+  var fn = functions[i];
+  if (fn.type == "code")
+    fn.code = parseCode(fn);
+}
+
+function parseNode () {
+  var n = readInt();
+  if (n & 1) return n>>1;
+  if (n & 2) {
+    n = n >> 2;
+    var str = "";
+    while (n > 0) {
+      byte = readByte();
+      str += String.fromCharCode(byte);
+      n--;
+    }
+    return str;
+  }
+  return parseN(n>>2, parseNode);
+}
+
+var metadata = parseNode();
+
+return {
+  modules: modules,
+  types: types,
+  functions: functions,
+  metadata: metadata,
+};
+
+}
