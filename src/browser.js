fa93c4c529f2ef0786cd92ce219d1bb773006783
Arnaud Castellanos Galea <poderosoarnaud@gmail.com>
2019-04-21 18:29:18 -0500

    src directory, package.json, remove auro.js

--- src/browser.js
+++ src/browser.js
@@ -1,11 +1,20 @@
 
-var Auro = require("./auro.js")
+var state = require("./state.js")
 var Compiler = require("./compiler.js")
 
+var compiled_modules = {}
+
+var Auro = {
+  modules: {},
+  compile_module: function () {
+    
+  }
+}
 Auro.escape = Compiler.escape
 Auro.compile = Compiler.compile
 
 if (typeof window === "undefined")
   throw new Error("Not running in a browser")
+
 
 window.Auro = Auro
